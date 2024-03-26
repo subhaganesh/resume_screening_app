a0cf237c8e99602cb61daeddb88bf6a8ef2c91dd
subhaganesh <subhaganesh5@outlook.com>
2024-03-26 23:46:39 +0530

    update

--- app.py
+++ app.py
@@ -37,39 +37,8 @@ def main():
         prediction_id = clf.predict(input_features)[0]
         st.write(prediction_id)
 
-       ''' # Map category ID to category name
-        category_mapping = {
-            15: "Java Developer",
-            23: "Testing",
-            8: "DevOps Engineer",
-            20: "Python Developer",
-            24: "Web Designing",
-            12: "HR",
-            13: "Hadoop",
-            3: "Blockchain",
-            10: "ETL Developer",
-            18: "Operations Manager",
-            6: "Data Science",
-            22: "Sales",
-            16: "Mechanical Engineer",
-            1: "Arts",
-            7: "Database",
-            11: "Electrical Engineering",
-            14: "Health and fitness",
-            19: "PMO",
-            4: "Business Analyst",
-            9: "DotNet Developer",
-            2: "Automation Testing",
-            17: "Network Security Engineer",
-            21: "SAP Developer",
-            5: "Civil Engineer",
-            0: "Advocate",
-        }
-
-        category_name = category_mapping.get(prediction_id, "Unknown")
-
-        st.write("Predicted Category:", category_name)'''
-
+        if prediction_id != prediction_id:
+            st.write('unknown')
 
 
 # python main
